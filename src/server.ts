54ee5eb7d20a539f26ff8e958637942c2f2574bc
welschoff <h.welschoff@gmx.de>
2021-10-29 10:50:40 +0200

    Add GET and POST function

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,11 @@ if (!process.env.MONGODB_URI) {
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get('/api/characters/', async (_request, response) => {
-  const charactersCollection = getCharacterCollection();
-  const characters = charactersCollection.find();
+  const characterCollection = getCharacterCollection();
+  const characters = characterCollection.find();
   const allCharacters = await characters.toArray();
   response.send(allCharacters);
 });
@@ -23,3 +24,27 @@ connectDatabase(process.env.MONGODB_URI).then(() =>
     console.log(`Example app listening at http://localhost:${port}`);
   })
 );
+
+app.post('/api/characters', async (request, response) => {
+  const characterCollection = getCharacterCollection();
+  const newCharacter = request.body;
+
+  if (
+    typeof newCharacter.name !== 'string' ||
+    typeof newCharacter.house !== 'string' ||
+    typeof newCharacter.dateOfBirth !== 'string'
+  ) {
+    response.status(404).send('Missing properties');
+  }
+  const isCharacterKnown = await characterCollection.findOne({
+    name: newCharacter.name,
+  });
+  if (isCharacterKnown) {
+    response
+      .status(409)
+      .send(`There is already someone called ${newCharacter.name}`);
+  } else {
+    characterCollection.insertOne(newCharacter);
+    response.send(`${newCharacter.name} was added`);
+  }
+});
