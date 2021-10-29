7f87da9378c367ebffd94a60ae5c6a94e5a88d91
welschoff <h.welschoff@gmx.de>
2021-10-29 10:32:36 +0200

    :sparkles: Add getCharacters route
    Add getCharacters route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
+import { connectDatabase } from './utils/database';
+import { getCharacterCollection } from './utils/database';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('No MongoDB URI dotenv variable');
+}
 
 const app = express();
 const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
+app.get('/api/characters/', async (_request, response) => {
+  const charactersCollection = getCharacterCollection();
+  const characters = charactersCollection.find();
+  const allCharacters = await characters.toArray();
+  response.send(allCharacters);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+connectDatabase(process.env.MONGODB_URI).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
