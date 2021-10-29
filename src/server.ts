42580c50972bfaf00e263cbaddbe13a1b8146974
welschoff <h.welschoff@gmx.de>
2021-10-29 15:27:50 +0200

    :sparkles: Add update functions
    Add update functions

--- src/server.ts
+++ src/server.ts
@@ -50,10 +50,47 @@ app.delete('/api/characters/:name', async (request, response) => {
     name: character,
   });
   if (!characterRequest) {
-    response.status(404).send('User not found.');
+    response.status(404).send('Character not found.');
   } else {
     characterCollection.deleteOne(characterRequest);
     response.send(`${character} was deleted.`);
+  }
+});
+
+// Insert fields
+app.post('/api/characters/:name', async (request, response) => {
+  const characterCollection = getCharacterCollection();
+  const newField = request.body;
+  const character = request.params.name;
+  const characterRequest = await characterCollection.findOne({
+    name: character,
+  });
+  if (characterRequest) {
+    await characterCollection.updateOne(
+      { name: character },
+      { $set: newField }
+    );
+    response.send('Updated');
+  } else {
+    response.status(404).send('Character not found');
+  }
+});
+
+app.put('/api/characters/:name', async (request, response) => {
+  const characterCollection = getCharacterCollection();
+  const deleteField = request.body;
+  const character = request.params.name;
+  const characterRequest = await characterCollection.findOne({
+    name: character,
+  });
+  if (characterRequest) {
+    await characterCollection.updateOne(
+      { name: character },
+      { $unset: deleteField }
+    );
+    response.send('Updated');
+  } else {
+    response.status(404).send('Character not found');
   }
 });
 
